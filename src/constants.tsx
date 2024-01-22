bb33f3fff547c9ce6f841026542b7bfdec481c85
dalapto <dmcalister11@outlook.com>
2024-01-22 09:56:44 +0000

    constants enums & names

---
+++ src/constants.tsx
@@ -0,0 +1,39 @@
+export enum SUITS {
+	Clubs = 'clubs',
+	Diamonds = 'diamonds',
+	Hearts = 'hearts',
+	Spades = 'spades',
+}
+
+enum PIP_ORDER {
+	Ace = 'ace',
+	Two = 'two',
+	Three = 'three',
+	Four = 'four',
+	Five = 'five',
+	Six = 'six',
+	Seven = 'seven',
+	Eight = 'eight',
+	Nine = 'nine',
+	Ten = 'ten',
+	Jack = 'jack',
+	Queen = 'queen',
+	King = 'king',
+	Joker = 'joker',
+}
+
+enum POWER_ORDER {
+	Joker = 'joker',
+	Two = 'two',
+	Jack = 'jack',
+	Queen = 'queen',
+	Eight = 'eight',
+}
+
+const JOKERS_FOLDER = 'jokers';
+const CLUBS_FOLDER = 'clubs';
+const DIAMONDS_FOLDER = 'diamonds';
+const HEARTS_FOLDER = 'hearts';
+const SPADES_FOLDER = 'spades';
+
+const BACKCARD_FILE = 'reverse_back';
