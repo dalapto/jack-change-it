2469dda612f069befe254a5c7ae399cbb4748c28
dalapto <dmcalister11@outlook.com>
2024-01-23 19:43:19 +0000

    image path handling for prod

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ASSETS_FOLDER } from '../constants';
 
 function CardImage({ src, ...dependencies }): React.JSX.Element {
-	if (process.env.NODE_ENV === 'production') {
-		src = `/${ASSETS_FOLDER}/${src}`; // in prod image path is different, this ensures images load
+	if (process.env.NODE_ENV == 'prod') {
+		src = `/jack-change-it${src}`; // in prod image path is different, this ensures images load
 	}
 	return <img src={src} draggable="false" style={{ height: 'inherit', width: 'inherit' }} {...dependencies} />;
 }
